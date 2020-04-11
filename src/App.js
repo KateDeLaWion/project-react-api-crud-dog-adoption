4d22cfd04734c792682c104b5f56662db74a79ce
Student101 <student101@PSAdmins-Air-3.home>
2020-04-10 20:12:25 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+import Home from './components/Home' ;
+import DogTab from './components/DogTab' ;
+import Crud from './components/Crud' ;
+
+
+export default class App extends Component {
+  
+
+  render() {
+    
+    return(
+      <div>
+        
+          <Router>
+
+            <nav>   
+              <Link to="/home">Home</Link> #####
+              <Link to="/friends">Our furry friends</Link>##### 
+              <Link to="/reviews">Reviews</Link> 
+            </nav>
+ 
+            <Switch> 
+
+              <Route exact path="/home">
+                <Home />
+              </Route>
+              <Route exact path="/friends">
+                <DogTab />
+              </Route>
+              <Route exact path="/reviews">
+                <Crud />
+              </Route>
+                         
+            </Switch>
+
+          </Router>
+
+        
+      </div>
+     )
+    
+  }
+}
