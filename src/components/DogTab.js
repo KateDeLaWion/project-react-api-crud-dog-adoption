42340a0b95284b773c8dbbc2051487f1faef6417
Student101 <student101@PSAdmins-Air-3.home>
2020-04-12 13:22:44 -0400

    new arrangement

--- src/components/DogTab.js
+++ src/components/DogTab.js
@@ -1,1 +1,50 @@
-// change on construction
+import React, { Component } from 'react';
+import DogPhoto from './DogPhoto';
+import './DogTab.css';
+import Spin from 'react-reveal/Spin';
+
+class App extends Component {
+
+constructor(props) {
+
+  super(props)
+
+  this.state = {
+     dogImages: []
+  }
+  
+}
+
+componentDidMount() {
+  fetch("https://dog.ceo/api/breeds/image/random/20")
+  .then(res => res.json())
+  .then((data) => {
+    this.setState({ dogImages: data.message })
+  })
+}
+
+render() {
+
+  const dogList = this.state.dogImages.map((url, i) => {
+    return <DogPhoto key={i} url={url}/>
+  })
+
+  return (
+    <div className="Tab">
+      <Spin>
+      <h1 className="neon">Our furry friends in house waiting for adoption </h1>
+      </Spin>
+      {dogList}
+    </div>
+  );
+
+}
+
+
+}
+
+export default App;
+
+
+
+
