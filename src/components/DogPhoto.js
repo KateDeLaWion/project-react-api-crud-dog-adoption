5590b9bd42c6714307270dbf5bae5efa13f12151
Student101 <student101@PSAdmins-Air-3.home>
2020-04-12 13:30:04 -0400

    this component is modified

--- src/components/DogPhoto.js
+++ src/components/DogPhoto.js
@@ -1,1 +1,13 @@
-// new change coming
+import React from 'react'
+import './DogPhoto.css';
+import Zoom from 'react-reveal/Zoom'; // Importing Zoom effect
+
+export default function DogPhoto(props) {
+    return (
+        <div>
+            <Zoom>
+            <img src={props.url} className="dogPhotoSize"/>
+            </Zoom>
+        </div>
+    )
+}
